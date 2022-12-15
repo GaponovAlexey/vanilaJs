780604466f8e9adc6b976a53539d5150bbb3e82e
kekelberi <alexei.gaponov@gmail.com>
2022-12-15 16:33:27 -0600

    saves

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-import './style.css'
+document.querySelector("#app").innerHTML = `<div>da</div>`;
 
-document.querySelector('#app').innerHTML = `
-  <div>
-  start
-  </div>
-`
+const main = (document.querySelector(
+  "main"
+).innerHTML = `<div class="container" >
+<p  class="draggable" draggable="true" >1</p>
+<p  class="draggable" draggable="true" >2</p>
+</div>`);
 
+const da = document.querySelectorAll(".container");
 
