1fbd1766b60e6a743807709d7635b473909506c3
kekelberi <alexei.gaponov@gmail.com>
2022-12-15 16:56:21 -0600

    save

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-document.querySelector("#app").innerHTML = `<div>da</div>`;
+const app = document.querySelector("#app").innerHTML = `<div>da</div>`;
 
-const main = (document.querySelector(
-  "main"
-).innerHTML = `<div class="container" >
-<p  class="draggable" draggable="true" >1</p>
-<p  class="draggable" draggable="true" >2</p>
-</div>`);
-
-const da = document.querySelectorAll(".container");
-
+const da = $("#app")
+console.log("app", da);
