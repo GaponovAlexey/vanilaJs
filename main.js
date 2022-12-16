32b2073e575f06b50407e5ddf43c21139624e2b2
kekelberi <alexei.gaponov@gmail.com>
2022-12-16 14:36:05 -0600

    save

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 document.querySelector("h1").innerHTML = `<div>name</div>`;
 
 jQuery(document).ready(function () {
-  // const l = $("h1");
+  $("h1").click(function() {
+    $(this).hide()
+  })
 
 });
